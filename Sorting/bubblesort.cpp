b0217e5a849cef421105a005559f47ebb12addc4
Aditya Ranjan <aditya74810@gmail.com>
2021-12-29 01:38:14 +0530

    Adding STL file

---
+++ Sorting/bubblesort.cpp
@@ -0,0 +1,76 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// void selectionsort(int a[],int n)
+// {
+//     for(int i=0;i<n;i++)
+//     {
+//        for(int j=0;j<n-i;j++)
+//        {
+//           if(a[j]>a[j+1])
+//           {
+            
+//               swap(a[j+1],a[j]);
+//           }
+//        }
+//     }
+//     for(int i=0;i<n;i++)
+//     {
+//         cout<<a[i]<<" ";
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int a[n];
+//     for(int i=0;i<n;i++)
+//     {
+//         cin>>a[i];
+//     }
+
+//     selectionsort(a,n);
+// }
+
+// optimised code of bubble sort
+#include<bits/stdc++.h>
+using namespace std;
+void selectionsort(int a[],int n)
+{
+    int count=0;
+    for(int i=1;i<n;i++)
+    {
+        bool swapped=false;
+       for(int j=0;j<n-i;j++)
+       {
+          if(a[j]>a[j+1])
+          {
+            
+              swap(a[j+1],a[j]);
+              count++;
+              swapped=true;
+          }
+       }
+       if(swapped==false)
+       {
+           break;
+       }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" "<<count;
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    selectionsort(a,n);
+}
---
+++ Sorting/selectionsort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selectionsort(int a[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int minIndex=i;
+       for(int j=i+1;j<n;j++)
+       {
+          if(a[j]<a[minIndex])
+          {
+            //   minIndex=j;
+              swap(a[j],a[i]);
+          }
+       }
+    //    swap(a[minIndex],a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    selectionsort(a,n);
+}
